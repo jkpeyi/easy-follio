2d1fc07a02146b1bb556a54b8d4664745126a158
jkpeyi <jkpeyi@gmail.com>
2024-03-28 14:44:20 +0100

    add: text_to_html_node function to TextNode Class

--- src/textnode.py
+++ src/textnode.py
@@ -1,3 +1,4 @@
+from leafnode import LeafNode
 class TextNode():
 
     def __init__(self,text, text_type, url=None):
@@ -12,3 +13,18 @@ class TextNode():
     
     def __repr__(self) -> str:
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
+    
+    def text_node_to_html_node(self):
+        convObj={
+          "text" : LeafNode(None, self.text),
+          "bold" : LeafNode('b',self.text),
+          "italic" : LeafNode("i", self.text),
+          "code" :LeafNode("code",self.text),
+          "link": LeafNode("a",self.text, {"href":""}),
+          "image": LeafNode("img", "", {"href":"", "alt":self.text})
+        }
+        
+        if self.text_type not in convObj:
+            raise Exception('Not a known text type')
+        
+        return convObj[self.text_type]
